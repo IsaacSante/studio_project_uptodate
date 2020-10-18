4640bee60fdc265185723864efb4913d2477040a
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2020-10-18 01:21:42 -0400

    modified cube

--- site.js
+++ site.js
@@ -6,9 +6,20 @@ var modal_desc = document.getElementById("modal-desc");
 var modal_img = document.getElementById("modal-img"); 
 var modal_name = document.getElementById("modal-name"); 
 var close = document.getElementById("close");
-
 var cubeCount, material, raycaster;
 var cube = [];
+
+
+// function closeDiv()
+// {
+//     var Temp = document.getElementById("paperuploaded")
+//     if (Temp != null)
+//         Temp.style.display = "none";
+// }
+
+// window.setTimeout("closeDiv();", 5000);
+
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
   75,
@@ -19,37 +30,28 @@ var camera = new THREE.PerspectiveCamera(
 
 var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
- renderer.setClearColor( 0xffffff, 0);
+renderer.setClearColor( 0xffffff, 0);
 document.body.appendChild(renderer.domElement);
 const color = 0xffffff;
 const intensity = 1.3;
 const light = new THREE.AmbientLight(color, intensity);
 scene.add(light);
 
-// var otherlight = new THREE.DirectionalLight( 0xffff00, 1 );
-// 				otherlight.position.set( 1000, 1000, 1000 );
-// 				scene.add( otherlight );
-
 var light1 = new THREE.PointLight(0xffffff, 4, 50);
 light1.position.set(0,300,500);
 scene.add(light1)
 
-// var geometry = new THREE.SphereBufferGeometry(0.3, 32, 16);
 var geometry = new THREE.BoxBufferGeometry( 0.5, 0.5, 0.5 );
 camera.position.z = 5;
 
 var controls = new OrbitControls(camera, renderer.domElement);
-controls.update();
-
-controls.minDistance = 0.2;
-controls.maxDistance = 15;
+controls.update()
+controls.minDistance = 1;
+controls.maxDistance = 50;
 controls.enableDamping = true;
 controls.dampingFactor = 0.2;
 
-var mouse = new THREE.Vector2(),
-  INTERSECTED;
-var radius = 100,
-  theta = 0;
+var mouse = new THREE.Vector2(), INTERSECTED;
 raycaster = new THREE.Raycaster();
 document.addEventListener("mousemove", onDocumentMouseMove, false);
 
@@ -65,13 +67,10 @@ var base = new Airtable({ apiKey: "keyMKnZBFsdFtC0UX" }).base(
 );
 
 cubeCount = 0;
-
 base("Table 1")
   .select({
     maxRecords: 100,
     view: "Grid view",
-    //sort: [{ field: "Total", direction: "desc" }],
-    //  filterByFormula: "IF({Mostly collected during colonial period},'True')",
   })
   .eachPage(
     function page(records, fetchNextPage) {
@@ -84,9 +83,9 @@ base("Table 1")
         cube[cubeCount].position.x = Math.random() * 2 - 1;
         cube[cubeCount].position.y = Math.random() * 10 - 1;
         cube[cubeCount].position.z = Math.random() * 6 - 1;
-        cube[cubeCount].scale.x = Math.random() + 0.1;
-        cube[cubeCount].scale.y = Math.random() + 0.1;
-				cube[cubeCount].scale.z = Math.random() + 0.1;
+        cube[cubeCount].rotation.x = Math.random() * 2 * Math.PI;
+        cube[cubeCount].rotation.y = Math.random() * 2 * Math.PI;
+        cube[cubeCount].rotation.z = Math.random() * 2 * Math.PI;
         cube[cubeCount].userData = record.fields;
         scene.add(cube[cubeCount]);
       });
@@ -101,57 +100,44 @@ base("Table 1")
     }
   );
 
-
 function animate() {
   requestAnimationFrame(animate);
   var timer = 0.0001 * Date.now();
-  for (var i = 1, il = cube.length; i < il; i++) {
-    cube[i].position.x = 8 * Math.cos(timer + i);
-    cube[i].position.y = 5 * Math.sin(timer + i * 1.5);
-    cube[i].position.z = 5 * Math.tan(timer + i *1.1);
-    // cube[i].scale.x = 1 * Math.sin(timer + i);
-    // cube[i].scale.y = 5 * Math.cos(timer + i *1.2);
-    //cube[i].scale.z = 5 * Math.sin(timer + i);
-  }
-
   raycaster.setFromCamera(mouse, camera);
   var intersects = raycaster.intersectObjects(scene.children);
 
   if (intersects.length > 0) {
-    if (INTERSECTED != intersects[0].object) {
-      //	if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-      INTERSECTED = intersects[0].object;
-      console.log(INTERSECTED.userData);
-      //  console.log(modal_container)
+     if (INTERSECTED != intersects[0].object) {
+     INTERSECTED = intersects[0].object;
       modal_container.classList.add("show");
       modal_title.innerHTML = INTERSECTED.userData.artid;
       modal_name.innerHTML = INTERSECTED.userData.aname; 
       modal_desc.innerHTML = INTERSECTED.userData.artdes;
       modal_img.src = INTERSECTED.userData.manimg;
-      // INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-      //INTERSECTED.material.emissive.setHex( 0xff0000 );
+      INTERSECTED.scale.x 
     }
   } else {
-    //if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
     INTERSECTED = null;
-  }
+   }
 
+ for (var i = 1, il = cube.length; i < il; i++) {
+    cube[i].position.x = 8 * Math.cos(timer + i) 
+    cube[i].position.y = 5 * Math.sin(timer + i * 1.5)
+    cube[i].position.z = 5 * Math.tan(timer + i *1.1) 
+  }
   controls.update();
   renderer.render(scene, camera);
 }
 animate();
 
 window.addEventListener( 'resize', onWindowResize, false );
-
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
-  
 }
 
 close.addEventListener("click", () => {
-  //   console.log('working fuck')
   modal_container.classList.remove("show");
 });
 
