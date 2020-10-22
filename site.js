6777a8941339074b3abfb5d2344eda0d6d18451f
Isaac Sante <43215955+IsaacSante@users.noreply.github.com>
2020-10-22 17:44:27 -0400

    changes to code

--- site.js
+++ site.js
@@ -1,51 +1,70 @@
 import * as THREE from "./three.module.js";
 import { OrbitControls } from "./OrbitControls.js";
+//import * as TWEEN from '/Tween';
+
 var modal_container = document.getElementById("modal-container");
 var modal_title = document.getElementById("modal-title");
-var modal_desc = document.getElementById("modal-desc"); 
-var modal_img = document.getElementById("modal-img"); 
-var modal_name = document.getElementById("modal-name"); 
+var modal_desc = document.getElementById("modal-desc");
+var modal_img = document.getElementById("modal-img");
+var modal_name = document.getElementById("modal-name");
 var close = document.getElementById("close");
-var cubeCount, material, raycaster;
+var cubeCount, material, raycaster, geometry;
+var scene, camera, renderer, controls;
 var cube = [];
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+function initScene() {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color("White");
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor( 0xffffff, 0);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  SceneLighting();
+  SceneControls();
+ // geoHandler();
+}
 
-document.body.appendChild(renderer.domElement);
-const color = 0xffffff;
-const intensity = 1.3;
-const light = new THREE.AmbientLight(color, intensity);
-scene.add(light);
+function SceneLighting() {
+  var light1 = new THREE.PointLight(0xffffff, 4, 50);
+  light1.position.set(0, 300, 500);
+  scene.add(light1);
 
-var light1 = new THREE.PointLight(0xffffff, 4, 50);
-light1.position.set(0,300,500);
-scene.add(light1)
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+  directionalLight.position.set(5, 5, 10);
 
-//var geometry = new THREE.BoxBufferGeometry( 0.5, 0.5, 0.5 );
-var geometry = new THREE.SphereBufferGeometry(0.3, 32, 16);
-camera.position.z = 5;
+  const directionalLightHelper = new THREE.DirectionalLightHelper(
+    directionalLight,
+    5
+  );
+  const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 3);
+  scene.add(directionalLight, directionalLightHelper, hemisphereLight);
+}
 
-var controls = new OrbitControls(camera, renderer.domElement);
-controls.update()
-controls.minDistance = 1;
-controls.maxDistance = 50;
-controls.enableDamping = true;
-controls.dampingFactor = 0.2;
+function SceneControls() {
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+  controls.minDistance = 1;
+  controls.maxDistance = 50;
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
+  camera.position.z = 5;
+}
 
-var mouse = new THREE.Vector2(), INTERSECTED;
+var mouse = new THREE.Vector2(),
+  INTERSECTED;
 raycaster = new THREE.Raycaster();
-
 document.addEventListener("click", onDocumentMouseClick, false);
+
+geometry = new THREE.PlaneBufferGeometry(1.5, 1.5);
+
+
+
 
 function onDocumentMouseClick(event) {
   event.preventDefault();
@@ -54,13 +73,24 @@ function onDocumentMouseClick(event) {
 }
 
 
+
+function scaleUp() {
+
+
+}
+
+function scaleDown() {
+  
+}
+
+
 var Airtable = require("airtable");
 var base = new Airtable({ apiKey: "keyMKnZBFsdFtC0UX" }).base(
-  'app7x77PDdblrFWMR'
+  "app7x77PDdblrFWMR"
 );
 
 cubeCount = 0;
-base("Table 1")
+base("Table 3")
   .select({
     maxRecords: 50,
     view: "Grid view",
@@ -69,6 +99,7 @@ base("Table 1")
     function page(records, fetchNextPage) {
       records.forEach(function (record) {
         cubeCount++;
+        //var texture = new THREE.TextureLoader().load(record.fields.Img);
         material = new THREE.MeshBasicMaterial({
           color: Math.random() * 0xffffff,
         });
@@ -80,21 +111,29 @@ base("Table 1")
         cube[cubeCount].rotation.y = Math.random() * 2 * Math.PI;
         cube[cubeCount].rotation.z = Math.random() * 2 * Math.PI;
         cube[cubeCount].userData = record.fields;
-        // if (cube[cubeCount].userData.Collecteddcp){
-        //   geometry = new THREE.SphereBufferGeometry(0.3, 32, 16);
-        // }
+
+        //source code 
+        //https://stackoverflow.com/questions/31229214/manipulate-objects-in-the-browser-with-three-js-using-the-mouse
+        // cube[cubeCount].userData.scaleUp = function(h) {
+        //           if (h.userData.scaleDownTween) h.userData.scaleDownTween.stop();
+        //           let initScale = h.scale.clone();
+        //           let finalScale = new THREE.Vector3().setScalar(2);
+        //           h.userData.scaleUpTween = new TWEEN.Tween(initScale).to(finalScale, 500).onUpdate(function(obj) {
+        //             h.scale.copy(obj)
+        //           }).start();
+        //         }
+        //         cube[cubeCount].scaleDown = function(h) {
+        //           if (h.userData.scaleUpTween) h.userData.scaleUpTween.stop();
+        //           let initScale = h.scale.clone();
+        //           let finalScale = new THREE.Vector3().setScalar(1);
+        //           h.userData.scaleUpTween = new TWEEN.Tween(initScale).to(finalScale, 500).onUpdate(function(obj) {
+        //             h.scale.copy(obj)
+        //           }).start();
+        //         }
         scene.add(cube[cubeCount]);
-        if (record.fields.Collecteddcp === true) {
-          geometry = new THREE.BoxBufferGeometry( 0.5, 0.5, 0.5 );
-         // geometry = new THREE.SphereBufferGeometry(0.3, 32, 16);
-         // element.classList.add('highlighted');
-        }
-
-
       });
       fetchNextPage();
     },
-
     function done(err) {
       if (err) {
         console.error(err);
@@ -108,46 +147,42 @@ function animate() {
   var timer = 0.0001 * Date.now();
   raycaster.setFromCamera(mouse, camera);
   var intersects = raycaster.intersectObjects(scene.children);
-
   if (intersects.length > 0) {
-     if (INTERSECTED != intersects[0].object) {
-     INTERSECTED = intersects[0].object;
+    if (INTERSECTED != intersects[0].object) {
+      INTERSECTED = intersects[0].object;
       modal_container.classList.add("show");
-      console.log(INTERSECTED.userData)
-      modal_title.innerHTML = INTERSECTED.userData.Country;
-      modal_name.innerHTML = INTERSECTED.userData.Total; 
-      modal_desc.innerHTML = INTERSECTED.userData.Before;
-      //  INTERSECTED.scale.x = INTERSECTED.scale.x * 2;
-      //  INTERSECTED.scale.y = INTERSECTED.scale.y * 2;
-      //  INTERSECTED.scale.z = INTERSECTED.scale.z * 2;
+      //if (INTERSECTED) INTERSECTED.userData.scaleDown(INTERSECTED);
+     // INTERSECTED.userData.scaleUp(INTERSECTED);
+      modal_title.innerHTML = INTERSECTED.userData.Title;
+      modal_name.innerHTML = INTERSECTED.userData.Country;
+      modal_desc.innerHTML = INTERSECTED.userData.Description;
+      modal_img.src = INTERSECTED.userData.Img;
     }
   } else {
+   // if (INTERSECTED) INTERSECTED.userData.scaleDown(INTERSECTED);
     INTERSECTED = null;
-    // INTERSECTED.scale.x = INTERSECTED.scale.x / 2;
-    // INTERSECTED.scale.y = INTERSECTED.scale.y / 2;
-    // INTERSECTED.scale.z = INTERSECTED.scale.z / 2;
-   }
-
- for (var i = 1, il = cube.length; i < il; i++) {
-    cube[i].position.x = 8 * Math.cos(timer + i) 
-    cube[i].position.y = 5 * Math.sin(timer + i * 1.5)
-    cube[i].position.z = 5 * Math.tan(timer + i *1.1) 
   }
 
+  for (var i = 1, il = cube.length; i < il; i++) {
+    cube[i].position.x = 8 * Math.cos(timer + i);
+    cube[i].position.y = 5 * Math.sin(timer + i * 1.5);
+    cube[i].position.z = 5 * Math.tan(timer + i * 1.1);
+  }
   controls.update();
+  TWEEN.update();
   renderer.render(scene, camera);
 }
+
+initScene();
 animate();
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 close.addEventListener("click", () => {
   modal_container.classList.remove("show");
 });
-
-
